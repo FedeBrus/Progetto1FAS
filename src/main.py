2cb00a4696d0ac23eff03c1e543eed2fa604b64f
FedeBrus <federico.brusadin@gmail.com>
2026-04-11 12:57:16 +0200

    Feature: added average query logic

--- src/main.py
+++ src/main.py
@@ -1,25 +1,23 @@
 import argparse
 import sys
 from loader import load_data
+from stats import *
 
 def parse_args():
     parser = argparse.ArgumentParser(prog="wals_analyze", description="WALS Data Analysis Tool")
     
     subparsers = parser.add_subparsers(dest="command", help="Available subcommands", required=True)
 
-    # --- aggregate ---
-    parser_aggregate = subparsers.add_parser("aggregate")
+    # --- average ---
+    parser_average = subparsers.add_parser("average")
     
-    parser_aggregate.add_argument("metric", choices=["family", "country"])
-    parser_aggregate.add_argument("aggregation", choices=["average", "median"])
-    parser_aggregate.add_argument("feature")
+    parser_average.add_argument("by", choices=["family", "country"])
+    parser_average.add_argument("feature")
 
-    group = parser_aggregate.add_mutually_exclusive_group()
-    group.add_argument("-a", "--ascending", action="store_true")
-    group.add_argument("-d", "--descending", action="store_true")
+    parser_average.add_argument("-d", "--descending", action="store_true")
     
-    parser_aggregate.add_argument("-f", "--filter-family", nargs='+')
-    parser_aggregate.add_argument("-c", "--filter-country", nargs='+')
+    parser_average.add_argument("-f", "--filter-family", nargs='+')
+    parser_average.add_argument("-c", "--filter-country", nargs='+')
 
     # --- count ---
     parser_count = subparsers.add_parser("count")
@@ -27,9 +25,7 @@ def parse_args():
     parser_count.add_argument("metric", choices=["family", "country"])
     parser_count.add_argument("item", choices=["countries", "families", "languages", "feature_value"])
 
-    group = parser_count.add_mutually_exclusive_group()
-    group.add_argument("-a", "--ascending", action="store_true")
-    group.add_argument("-d", "--descending", action="store_true")
+    parser_count.add_argument("-d", "--descending", action="store_true")
     
     parser_count.add_argument("-f", "--filter-family", nargs='+')
     parser_count.add_argument("-c", "--filter-country", nargs='+')
@@ -45,10 +41,18 @@ def main():
         print(f"Error: dataset file ({dataset_path}) not found")
         sys.exit(1)
         
-    print(df)
+    if args.command == "average":
+        average_frame = average(
+            df,
+            args.by,
+            args.feature,
+            descending=args.descending,
+            filter_family=args.filter_family,
+            filter_country=args.filter_country
+        )
 
-    if args.command == "aggregate":
-        pass
+        # plot = plot(average_frame)
+        # display(plot)
     
     elif args.command == "count":
         pass
---
+++ src/stats.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def average(df, metric, feature, descending=False, filter_family=[], filter_country=[]):
+    
+    if metric == "family":
+        metric = "Family"
+    elif metric == "country":
+        metric = "Country_ID"
+
+    query_df = df.copy()
+
+    if filter_family:
+        query_df = query_df[query_df["Family"].isin(filter_family)]
+
+    if filter_country:
+        query_df = query_df[query_df["Country_ID"].isin(filter_country)]
+    
+    query_df = query_df[query_df["Parameter_ID"] == feature]
+
+    query_df = query_df[[metric, "Number"]]
+
+    query_df = query_df.groupby(metric).mean()
+
+    query_df = query_df.sort_values(by="Number", ascending=not descending)
+    
+    print(query_df)
+    return query_df 
+
