436919efc92491ea71c08c931de5efbb9ed1ba4f
FedeBrus <federico.brusadin@gmail.com>
2026-04-11 11:30:06 +0200

    added argument parsing

---
+++ src/main.py
@@ -0,0 +1,49 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog="wals_analyze", description="WALS Data Analysis Tool")
+    
+    subparsers = parser.add_subparsers(dest="command", help="Available subcommands", required=True)
+
+    # --- aggregate ---
+    parser_aggregate = subparsers.add_parser("aggregate")
+    
+    parser_aggregate.add_argument("metric", choices=["family", "country"])
+    parser_aggregate.add_argument("aggregation", choices=["average", "median"])
+    parser_aggregate.add_argument("feature")
+
+    group = parser_aggregate.add_mutually_exclusive_group()
+    group.add_argument("-a", "--ascending", action="store_true")
+    group.add_argument("-d", "--descending", action="store_true")
+    
+    parser_aggregate.add_argument("-f", "--filter-family", nargs='+')
+    parser_aggregate.add_argument("-c", "--filter-country", nargs='+')
+
+    # --- count ---
+    parser_count = subparsers.add_parser("count")
+
+    parser_count.add_argument("metric", choices=["family", "country"])
+    parser_count.add_argument("item", choices=["countries", "families", "languages", "feature_value"])
+
+    group = parser_count.add_mutually_exclusive_group()
+    group.add_argument("-a", "--ascending", action="store_true")
+    group.add_argument("-d", "--descending", action="store_true")
+    
+    parser_count.add_argument("-f", "--filter-family", nargs='+')
+    parser_count.add_argument("-c", "--filter-country", nargs='+')
+    
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    # df = load_data("./dataset/features.csv")
+
+    if args.command == "aggregate":
+        pass
+    
+    elif args.command == "count":
+        pass
+
+if __name__ == "__main__":
+    main()
